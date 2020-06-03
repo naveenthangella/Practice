0938d8474bc0a9b85cb44ad776b993290e5dcd8f
naveenthangella <naveenthangella@gmail.com>
2020-06-02 22:39:41 -0700

    create an ec2 instance

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -3,10 +3,24 @@ provider "aws" {
 }
 
 resource "aws_instance" "practice" {
+  count = "2"
   ami = data.aws_ami.ami.image_id
   instance_type = "t2.micro"
+  key_name = "practice"
   tags = {
     Name  = "practice"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      type = ssh
+      host = self.private_ip
+      user = "centos"
+      private_key = file("/home/centos/.ssh/id_rsa")
+    }
+    inline = [
+      "yum install ansible -y"
+    ]
   }
 }
 
