e9064b986738ed0717d2bcb369fe4a2412670e92
naveenthangella <naveenthangella@gmail.com>
2020-06-04 01:07:07 -0700

    create an ec2 instance

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -27,9 +27,12 @@ resource "aws_instance" "practice" {
   }
 }
 
-resource "aws_s3_bucket" "HTML" {
+resource "aws_s3_bucket" "html" {
   bucket = "sample-html-naveen"
   acl = "public"
+  tags = {
+    Name = "my bucket"
+  }
 }
 resource "aws_s3_bucket_object" "Object" {
   bucket = "sample-html-naveen"
