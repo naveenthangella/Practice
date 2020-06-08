872e9f1998dc4a5bce00606303a196e299dad3d5
naveenthangella <naveenthangella@gmail.com>
2020-06-07 21:12:10 -0700

    create an ec2 instance

--- Ansible/Docker/docker.sh
+++ Ansible/Docker/docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum-config-manager --enable docker-ce-nightly
 sudo yum install docker-ce docker-ce-cli containerd.io
